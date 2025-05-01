9fb8d5ab6f8c88a063cbcea58249bd818801e9af
Claudenir Nojosa <clau.nojosaf@gmail.com>
2025-05-01 19:49:11 -0300

    tentando deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,33 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import nextPlugin from '@next/eslint-plugin-next';
+import globals from 'globals';
 
 export default tseslint.config(
   {
+    ignores: ['**/*.config.js', '**/*.config.mjs', 'node_modules/'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
     plugins: {
-      "@next/next": nextPlugin,
+      '@next/next': nextPlugin,
     },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   },
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    ...tseslint.configs.recommended,
-    ...nextPlugin.configs["core-web-vitals"],
+    files: ['**/*.ts', '**/*.tsx'],
+    ...nextPlugin.configs['core-web-vitals'],
   }
 );
