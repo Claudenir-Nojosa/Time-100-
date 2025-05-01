5f63975a473d6eb5c84c6722fe08b6f525f339b1
Claudenir Nojosa <clau.nojosaf@gmail.com>
2025-05-01 20:07:06 -0300

    tentando deploy

--- src/app/api/atividades/[id]/route.ts
+++ src/app/api/atividades/[id]/route.ts
@@ -3,10 +3,10 @@ import db from '@/lib/db';
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }  // Parâmetro como Promise
 ) {
   try {
-    const { id } = params;
+    const { id } = await params;  // Resolve a Promise
     
     await db.atividade.delete({
       where: { id }
