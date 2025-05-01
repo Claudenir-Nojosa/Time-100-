27cfe293b7913c2b7f6ad76fa52cc78a12bdb707
Claudenir Nojosa <clau.nojosaf@gmail.com>
2025-05-01 19:42:56 -0300

    tentando deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
-import tseslint from "typescript-eslint";
-import nextPlugin from "@next/eslint-plugin-next";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
 
 export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...nextPlugin.configs.recommended,
   {
     plugins: {
       "@next/next": nextPlugin,
